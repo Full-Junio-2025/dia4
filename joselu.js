45ffa257a6e21857017a3ca5aff87fe2aea6140e
armitayati <mita.002@gmail.com>
2025-07-03 14:26:12 +0200

    Revert "joseluis"
    This reverts commit 39b0d4406b4e7d48f9ee74eaaa238c3ac4316e1e.

--- joselu.js
+++
@@ -1,6 +0,0 @@
-let joseLuis = {
-    name: "Jose Luis Pineda",
-    age: 24,
-    profession: "Full Stack Developer",
-    skills: ["JavaScript", "HTML", "CSS", "Node.js", "React"],
-    hobbies: ["Reading", "Traveling", "Coding", "Gaming"],}
